302daae8c82079c98ea86ccb23393711af8a6d27
AarishSajid <161928829+AarishSajid@users.noreply.github.com>
2024-04-06 15:25:39 +0500

    Add files via upload

---
+++ Quiz_Game.py
@@ -0,0 +1,50 @@
+print("**** WELCOME TO QUIZ GAME ****")
+
+count = 0
+
+print("\nQUESTION 1")
+print("How many legs does a cat have? ")
+Answer = input("Answer : ")
+if  Answer == "4":
+    print ("GUD JOB")
+    count = count + 1
+else : 
+    print("GET GUD.")
+
+print("\nQUESTION 2")
+print("How many states are there in the US ?")
+Answer = input("Answer : ")
+if  Answer == "50":
+    print ("GUD JOB")
+    count = count + 1
+else : 
+    print("GET GUD.")
+
+print("\nQUESTION 3")
+print("How many types of blood do humans have?")
+Answer = input("Answer : ")
+if  Answer == "4":
+    print ("GUD JOB")
+    count = count + 1
+else : 
+    print("GET GUD.")
+
+print("\nQUESTION 4")
+print("How many variations of the color red can you see?")
+Answer = input("Answer : ")
+if  Answer == "100":
+    print ("GUD JOB")
+    count = count + 1
+else : 
+    print("GET GUD.")
+
+print("\nQUESTION 5")
+print("How much is 1+1?")
+Answer = input("Answer : ")
+if  Answer == "2":
+    print ("GUD JOB")
+    count = count + 1
+else : 
+    print("GET GUD.")
+
+print("\n\nQuestions answered correctly: ",count)
